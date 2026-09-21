520185d8e5ffe69598959f149630a0f6d0cb6a12
s-hivangi <07shivangisp@gmail.com>
2026-09-22 00:14:11 +0530

    frontend: add theme toggle icons

--- frontend/src/components/shared/OutlineIcon.jsx
+++ frontend/src/components/shared/OutlineIcon.jsx
@@ -10,6 +10,8 @@ const PATHS = {
   camera: 'M5 8a2 2 0 0 1 2-2h1l1-2h6l1 2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8Zm7 7a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z',
   check: 'm5 12 4 4L19 6',
   trash: 'M5 7h14M10 11v5M14 11v5M7 7l1 13h8l1-13M9 7l1-3h4l1 3',
+  sun: 'M12 3v2M12 19v2M3 12h2M19 12h2M5.6 5.6 7 7M17 17l1.4 1.4M18.4 5.6 17 7M7 17l-1.4 1.4M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0',
+  moon: 'M20 15.5A8.5 8.5 0 0 1 8.5 4 8.5 8.5 0 1 0 20 15.5Z',
 };
 
 export default function OutlineIcon({ name, size = 22, color = 'currentColor' }) {
